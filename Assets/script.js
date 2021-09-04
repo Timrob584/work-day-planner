b9d2fc346ca9aa646863ae44cd7fe1b5f0a745f1
Timrob584 <timrob584@gmail.com>
2021-09-04 14:33:08 -0400

    changed col 3 to save icon. Added on-click save to local storage function

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,25 @@
 // To Display Date at Top of Page
-var date = moment().format("dddd, MMMM do");
+var date = moment().format("dddd, MMMM do, YYYY");
 $("#currentDay").text(date);
+
+$(".time-block").each(function() {
+    var blockId = $(this).attr("id");
+    // load saved data from local storage
+    $("#" + blockId + " textarea").text(localStorage.getItem(moment().format("DDDYYYY") + blockId));
+  });
+
+// To update the text-area with data from local storage
+$(".text-area").each(function() {
+    var id = $(this).attr("id");
+    // load saved data from local storage
+    $("#" + id + " textarea").text(localStorage.getItem(moment().format("DDDYYYY") + blockId));
+  });
+
+// To Add On-Click for the Save Button
+var save = $(".save");
+save.on("click", save() {
+    var time = $(this).parent().attr("id");
+    var events = $(this).siblings(".events").val();
+
+    localStorage.setItem(time, schedule);
+});
