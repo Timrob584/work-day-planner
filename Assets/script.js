77f7d6809f9912f4260bbb08628cb486c3e58d54
Timrob584 <timrob584@gmail.com>
2021-09-05 15:52:48 -0400

    modified local storage set/getItem

--- Assets/script.js
+++ Assets/script.js
@@ -15,7 +15,7 @@ var task = $(this).siblings(".task").val();
 
 
 // To Display Date at Top of Page
-var date = moment().format("dddd-MMMM do");
+var date = moment().format("dddd MMMM do");
 $("#currentDay").text(date);
 
 // To Add On-Click for the Save Button
@@ -25,14 +25,14 @@ save.on("click", saveItem);
 // To Run Save Event
 function saveItem(e) {
     e.preventDefault();
-    localStorage.setItem(time, task);
+    localStorage.setItem(task);
 };
 
 // To update the text-area with data from local storage
 $(".time-block").each(function() {
   var blockId = $(this).attr("id");
 // Load saved data from local storage
-  $("#" + blockId + " textarea").text(localStorage.getItem(time, task));
+  $("#" + blockId + " textarea").text(localStorage.getItem(task));
 });
 
 // To set Past, Present, or Future
