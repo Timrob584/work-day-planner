9cb55cc31c02eeae40966b63728bd6ca978690d3
Timrob584 <timrob584@gmail.com>
2021-09-05 12:39:06 -0400

    Added hour variables, added ppf function, updated save event, added hour id to html blocks

--- Assets/script.js
+++ Assets/script.js
@@ -1,25 +1,55 @@
+var nine = $("#9");
+var ten = $("#10");
+var eleven = $("#11");
+var twelve = $("#12");
+var one = $("#1");
+var two = $("#2");
+var three = $("#3");
+var four = $("#4");
+var five = $("#5");
+
+var hours = [nine, ten, eleven, twelve, one, two, three, four, five];
+
 // To Display Date at Top of Page
-var date = moment().format("dddd, MMMM do, YYYY");
+var date = moment().format("dddd, MMMM do");
 $("#currentDay").text(date);
 
+// To update the text-area with data from local storage
 $(".time-block").each(function() {
     var blockId = $(this).attr("id");
-    // load saved data from local storage
+// Load saved data from local storage
     $("#" + blockId + " textarea").text(localStorage.getItem(moment().format("DDDYYYY") + blockId));
-  });
-
-// To update the text-area with data from local storage
-$(".text-area").each(function() {
-    var id = $(this).attr("id");
-    // load saved data from local storage
-    $("#" + id + " textarea").text(localStorage.getItem(moment().format("DDDYYYY") + blockId));
   });
 
 // To Add On-Click for the Save Button
 var save = $(".save");
-save.on("click", save() {
-    var time = $(this).parent().attr("id");
-    var events = $(this).siblings(".events").val();
+save.on("click", saveItem);
 
-    localStorage.setItem(time, schedule);
-});
+// To Run Save Event
+function saveItem(e) {
+    e.preventDefault();
+    var time = $(this).parent().attr("id");
+    var task = $(this).siblings(".task").val();
+
+    localStorage.setItem(time, task);
+};
+
+
+// To set Past, Present, or Future
+  function ppf() {
+    $(".time-block").each(function() {
+      var currentTime = moment().hours;
+  
+      if (currentTime < hours) {
+        $(this).addClass("past")
+      }
+      else if (currentTime === hours) {
+        $(this).addClass("present")
+      }
+      else {
+        $(this).addClass("future")
+      }
+    })
+  }
+
+ppf();
